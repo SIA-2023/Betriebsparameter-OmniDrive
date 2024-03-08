ea26ea8c65f8cd09d2ff96d38047435dfe51b282
SIA-2023 <156456223+SIA-2023@users.noreply.github.com>
2024-03-08 15:16:13 +0100

    Add files via upload

---
+++ demo/demo.ino
@@ -0,0 +1,32 @@
+#include "OmniDriver.h"
+
+OmniDriver driver;
+
+void setup(){
+  driver.distance_drive(200 /* Distance in cm */, 0 /* Fahrtrichtung in Grad */);
+
+  delay(2000);
+
+  driver.rotate_left(70 /* Speed in % */);
+
+  delay(2000);
+
+  driver.stop();
+  delay(500);
+  driver.rotate_right(70 /* Speed in % */);
+
+  delay(2000);
+  driver.stop();
+
+  delay(500);
+
+  driver.drive(180 /* Fahrtrichtung in Grad */, 80 /* Speed in % */);
+
+  delay(1000);
+
+  driver.stop();
+}
+
+void loop(){
+  delay(100);
+}
